9b0ee9f37cb888ea73494a7c0f2d5c84300de56d
Bryan Diaz <bryandiaz2010@yahoo.com>
2014-09-10 15:02:37 -0700

    Finished Basics of Crystal Ball

---
+++ app/src/main/java/android/diazb/crystalball/Predictions.java
@@ -0,0 +1,25 @@
+package android.diazb.crystalball;
+
+public class Predictions {
+
+    private static Predictions predictions;
+    private String[] answers;
+
+    private Predictions() {
+        answers=new String[] {
+                "You're wishes will come true.",
+                "You're wished will NEVER come true."
+        };
+    }
+
+    public static Predictions get(){
+        if(predictions==null) {
+            predictions=new Predictions();
+        }
+           return predictions;
+    }
+
+    public String getPrediction() {
+            return answers[1];
+    }
+}
